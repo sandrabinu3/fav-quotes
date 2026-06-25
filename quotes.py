647a2dec1a584a1d028a6ade84be5c01566827f1
sandrabinu3 <sandrabinu99@gmail.com>
2026-06-25 01:42:28 +0100

    my favourite quotes app code

---
+++ quotes.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql+psycopg2://postgres:010123@localhost/quotes"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class FavQuote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30), nullable=False)
+    quote = db.Column(db.String(2000), nullable=False)
+
+    def __repr__(self):
+        return f"<FavQuote {self.author}>"
+
+
+@app.route('/')
+def index():
+	result = FavQuote.query.all()
+	return render_template('index.html',result=result)
+
+
+@app.route('/quotes')
+def quotes():
+	 return render_template('quotes.html')
+
+@app.route('/process', methods =['POST'])
+def process():
+	author = request.form['author']
+	quote = request.form['quote']
+	quotedata =FavQuote(author=author,quote=quote)
+	db.session.add(quotedata)
+	db.session.commit()
+
+	return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
