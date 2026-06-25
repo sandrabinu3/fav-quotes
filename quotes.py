1d8e27a999c1badcfa8fc5bc8b20e8f2c585e56c
sandrabinu3 <sandrabinu99@gmail.com>
2026-06-25 02:03:44 +0100

    database table added

--- quotes.py
+++ quotes.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = (
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL",
     "postgresql+psycopg2://postgres:010123@localhost/quotes"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
